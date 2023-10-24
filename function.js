0869df0747d4f6911bccf5cfd77c89068c18deee
w-amon <xxx110680@gmail.com>
2023-10-24 12:20:05 +0300

    Answer Question 2

--- function.js
+++ function.js
@@ -18,3 +18,32 @@ function calculateArea(length, width) {
   const area2 = calculateArea(length2, width2);
   console.log(`The area of the rectangle is: ${area2}`);
   
+
+// Question 2: Scope Exploration
+// Declare a variable globalVar in the global scope and give it a value.
+// Inside a function, declare a variable localVar and give it a different value.
+// Attempt to access both globalVar and localVar within the function.
+// Write comments explaining your observations about variable scope.
+
+
+let globalVar = "I am a global variable"; // Declare a global variable in the global scope
+
+function myFunction() {
+  // Declare a local variable inside the function
+  let localVar = "I am a local variable";
+
+  // Access globalVar and localVar
+  console.log("Inside the function:");
+  console.log("globalVar: " + globalVar);
+  console.log("localVar: " + localVar);
+}
+
+// Call the function
+myFunction();
+
+// Attempt to access globalVar and localVar outside the function
+console.log("Outside the function:");
+console.log("globalVar: " + globalVar);
+
+// This will result in an error since localVar is not accessible outside the function.
+// console.log("localVar: " + localVar);
